ff60d6227544a680c7c4a0f08ddb999bdffa130e
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-02-28 19:03:06 -0300

    agregando

--- algoritmia.js
+++ algoritmia.js
@@ -103,3 +103,74 @@ function numerosPrimos(numero) {
   }
   return primos;
 }
+
+/**
+ * Calcular el factorial de un número:
+ */
+
+function factorial(numero) {
+  if (numero === 0 || numero === 1) {
+    return 1;
+  } else {
+    return numero * factorial(numero - 1);
+  }
+}
+
+/**
+ * Invertir una cadena:
+ */
+
+function invertirString(string) {
+  return string.split("").reverse().join("");
+}
+
+/**
+ * Calcular la secuencia de Fibonacci hasta un número dado:
+ */
+
+function fibonacci(numero) {
+  let fib = [0, 1];
+  for (let i = 2; i <= numero; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib.slice(0, numero + 1);
+}
+
+/**
+ * Encontrar el promedio de un arreglo de números:
+ */
+
+function promedioNumero(array) {
+  let suma = 0;
+  for (let i = 0; i < array.length; i++) {
+    suma += array[i];
+  }
+  return suma / array.length;
+}
+
+/**
+ * Convertir una cadena de texto a título (cada palabra comienza con una letra mayúscula):
+ */
+
+function textoAtitulo(string) {
+  let palabras = string.toLowerCase().split(" ");
+  for (let i = 0; i < palabras.length; i++) {
+    palabras[i] = palabras[i][0].toUpperCase() + palabras[i].slice(1);
+  }
+  return palabras.join(" ");
+}
+
+/**
+ * Verificar si una cadena contiene todas las letras del alfabeto:
+ */
+
+function verificarAbc(string) {
+  string = string.toLowerCase();
+  let alfabeto = "abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < alfabeto.length; i++) {
+    if (string.indexOf(alfabeto[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
