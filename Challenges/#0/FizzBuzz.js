fa8d14b71efa583e8dc6b948700252482d9fcac5
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-07-18 01:46:36 -0300

    new challenges

---
+++ Challenges/#0/FizzBuzz.js
@@ -0,0 +1,17 @@
+//lucas
+
+function fizzBuzz() {
+  for (let index = 0; index <= 100; index++) {
+    if (index % 3 == 0 && index % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (index % 3 === 0) {
+      console.log("fizz");
+    } else if (index % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(index);
+    }
+  }
+}
+
+fizzBuzz();
